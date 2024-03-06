dea95b763f1401c56c28db5338277be7f72322a6
Max <mx.jerg@gmail.com>
2024-03-06 14:52:52 +0100

    added sideways scroll for screenshots

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -27,3 +27,40 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+const slider = document.querySelector('.screenshots-slider');
+
+document.querySelectorAll('.screenshots-slider img').forEach(img => {
+    img.addEventListener('dragstart', e => {
+      e.preventDefault();
+    });
+  });
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2; // Scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+});
